8b88189746dee2183306a287c01b2d847e45ed93
hxchua00 <hxchua00@gmail.com>
2021-09-27 17:25:29 +0800

    Add project files.

---
+++ WeeklyAss3Q3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyAss3Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer T = new Timer();
+            T.StartCount();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WeeklyAss3Q3/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeeklyAss3Q3
+{
+    class Timer
+    {
+        private int count = 0;
+        bool IsCounterRunning = false;
+
+        public void StartCount()
+        {
+            IsCounterRunning = true;
+            count = 0;
+            while (IsCounterRunning)
+            {
+                Thread.Sleep(5000);
+                count++;
+                Console.WriteLine("Count added.");
+                Console.WriteLine();
+                Console.WriteLine("Stop count? y / n ");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "y":
+                        StopCount();
+                        break;
+                    default:
+                        break;
+
+                }
+
+            }
+            
+        }
+        public void StopCount()
+        {
+            if(IsCounterRunning == true)
+            {
+                IsCounterRunning = false;
+            }
+            else
+            {
+                return;
+            }
+            Console.WriteLine("Count: " + count);
+        }
+    }
+}
